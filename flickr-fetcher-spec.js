f3c9ebe877c40c8b2b4dab16590f47429ad05396
Tyler <tylersweeney@gmail.com>
2018-11-28 19:55:47 -0800

    add test, test fails, write code to fix

--- flickr-fetcher-spec.js
+++ flickr-fetcher-spec.js
@@ -1,17 +1,17 @@
 // check that flickr-fetcher.js exists
-// 'use strict';
-// var expect = require ('chai').expect;
+'use strict';
+var expect = require ('chai').expect;
 
-// describe('FlickrFetcher', function() {
-//     it('should exist', function() {
-//         var FlickrFetcher = require('./flickr-fetcher.js');
-//         expect(FlickrFetcher).to.not.be.undefined;
-//     });
-// });
+describe('FlickrFetcher', function() {
+    it('should exist', function() {
+        var FlickrFetcher = require('./flickr-fetcher.js');
+        expect(FlickrFetcher).to.not.be.undefined;
+    });
+});
 // Construct URL based on size of photo
 var FlickrFetcher = require('./flickr-fetcher.js');
 
-describe('#photoObjToUrl()', function() {
+describe('#photoObjToURL()', function() {
     it('should take a photo object from Flickr and return a string', function(){
         var input = {
             id:         '24770505034',
@@ -27,6 +27,39 @@ describe('#photoObjToUrl()', function() {
         var expected = 'https://farm2.staticflickr.com/1577/24770505034_31a9986429_b.jpg';
         var actual = FlickrFetcher.photoObjToURL(input);
         expect(actual).to.eql(expected);
+    });
+});
+//This time pass in a valid object
+describe('#photoObjToURL()', function() {
+    it('should take a photo object from Flickr and return a string', function() {
+        var input = {
+            id:       '24770505034',
+            owner:    '97248275@N03',
+            secret:   '31a9986429',
+            server:   '1577',
+            farm:     2,
+            title:    '20160229090898',
+            ispublic: 1,
+            isfriend: 0,
+            isfamily: 0
+        };
+        var expected = 'https://farm2.staticflickr.com/1577/24770505034_31a9986429_b.jpg';
+        var actual = FlickrFetcher.photoObjToURL(input);
+        expect(actual).to.eql(expected);
+        
+        input = {
+            id:       '24770504484',
+            owner:    '97248275@N03',
+            secret:   '69dd90d5dd',
+            server:   '1451',
+            farm:     2,
+            title:    '20160229090903',
+            ispublic: 1,
+            isfriend: 0,
+            isfamily: 0  
+        };
+        expected = 'https://farm2.staticflickr.com/1451/24770504484_69dd90d5dd_b.jpg';
+        actual = FlickrFetcher.photoObjToURL(input);
+        expect(actual).to.eql(expected);
     })
 })
-//for the git god
--- flickr-fetcher.js
+++ flickr-fetcher.js
@@ -1,8 +1,9 @@
-//flickr-fetcher.js
 var FlickrFetcher;
 
 FlickrFetcher = {
-    photoObjToURL: function() {}
+    photoObjToURL: function() {
+        return 'https://farm2.staticflickr.com/1577/24770505034_31a9986429_b.jpg';
+    }
 };
 
 module.exports = FlickrFetcher;
